b47331ed75214696cfa80361fa299c2ab8e4fb91
japneet-15 <jkaur2_be20@thapar.edu>
2022-03-12 17:22:31 +0530

    landing page added

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Timeline from "./components/Timeline";
+import Team from "./components/team";
+import Quote from "./components/quote";
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     <Home />
+     <Timeline />
+     <Team />
+     <Quote />
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ frontend/src/components/quote.js
@@ -0,0 +1,14 @@
+import "../css/quote.css"
+import collage from "../collage.jpg"
+
+const Quote = () => {
+    return ( 
+
+        <div className="container-5">
+        <img  className="collage-pic" src={collage} alt="collage"></img>
+        
+        </div>
+     );
+}
+ 
+export default Quote;
